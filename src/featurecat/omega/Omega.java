d93c9ee541663fc7559eec3ff0a0de0b3e570af2
featurecat <annie.wagner.cs@gmail.com>
2018-10-13 01:27:06 -0400

    omega main class

--- src/featurecat/omega/Omega.java
+++ src/featurecat/omega/Omega.java
@@ -1,11 +1,34 @@
 package featurecat.omega;
 
+import featurecat.omega.analysis.Leelaz;
+import featurecat.omega.rules.Board;
 import featurecat.omega.ui.OmegaFrame;
 
-public class Omega {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        new OmegaFrame();
+/**
+ * Main class.
+ */
+public class Omega {
+    public static OmegaFrame frame;
+    public static Leelaz leelaz;
+    public static Board board;
+    public static String version = "0.1";
+
+    /**
+     * Launches the game window, and runs the game.
+     */
+    public static void main(String[] args) throws IOException {
+        leelaz = new Leelaz();
+        leelaz.togglePonder();
+
+        board = new Board();
+        frame = new OmegaFrame();
+    }
+
+    public static void shutdown() {
+        leelaz.shutdown();
+        System.exit(0);
     }
 
 }
